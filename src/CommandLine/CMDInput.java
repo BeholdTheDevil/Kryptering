7e9fa2d6e993d7312228f8b8d07c59f8dc1a5403
BeholdTheDevil <antdeg@outlook.com>
2017-08-30 13:49:15 +0200

    Enkel commandline applikation som hanterar argument på olika sätt beroende på hur många argument som anges.

--- src/CommandLine/CMDInput.java
+++ src/CommandLine/CMDInput.java
@@ -1,7 +1,7 @@
 package CommandLine;
 
 /**
- * Created by anton on 2017-08-30.
+ * Created by Anton on 2017-08-30.
  */
 public class CMDInput {
 
@@ -13,15 +13,24 @@ public class CMDInput {
                 break;
 
             case 2:
-                System.out.println();
+                int a = 0;
+                int b = 0;
+                try {
+                    a = Integer.parseInt(args[1]);
+                    b = Integer.parseInt(args[2]);
+                } catch(NumberFormatException nfe) {
+                    System.out.println("Input var inte numeriskt. Avbryter...");
+                    System.exit(0);
+                }
+                System.out.println(a + b);
                 break;
 
             case 3:
-                System.out.println();
+                System.out.println(args[2] + " " + args[1] + " " + args[0]);
                 break;
 
             case 4:
-                System.out.println();
+                System.out.println("Jag orkar inte mer, jag stänger ner");
                 break;
 
             default:
